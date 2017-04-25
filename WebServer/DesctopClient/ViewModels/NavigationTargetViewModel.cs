fe82fc0ca09c900ba45a0b6b1935cd62af3eb080
alina.kurilova <adk12@tpu.ru>
2017-04-25 13:20:28 +0700

    теперь уже точно начало десктопа

---
+++ WebServer/DesctopClient/ViewModels/NavigationTargetViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using DesktopClient.Navigation;
+
+namespace DesktopClient.ViewModels
+{
+    public class NavigationTargetViewModel : BaseViewModel
+    {
+        public NavigationTargetViewModel()
+        {
+            InitializeNavigationCommands();
+        }
+
+        private void InitializeNavigationCommands()
+        {
+            /*NavigationCommands = new List<object>
+            {
+                new NavigationStateViewModel(OnNavigateCards, this is CardsViewModel, "Карты"),
+                new NavigationStateViewModel(OnNavigatePassports, this is PassportsViewModel, "Паспорта"),
+                new NavigationStateViewModel(OnNavigateProfile, this is ProfileViewModel, "Профиль"),
+                new NavigationStateViewModel(OnNavigateVisit, this is VisitViewModel, "Визит")
+            };*/           
+        }
+
+        public List<object> NavigationCommands { get; set; }
+    }
+}
